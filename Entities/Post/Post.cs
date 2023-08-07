4539265f912505b6bd387a4ca810f6d73b60551a
FarazJahanmanesh <jahanmaneshfaraz@gmail.com>
2023-08-06 22:05:03 -0700

    add some entities to entities folder and change the assemblies

---
+++ Entities/Post/Post.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities
+{
+    public class Post : BaseEntity
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int CategoryId { get; set; }
+        public Guid UserId { get; set; }
+        public User User { get; set; }
+        public Category Category { get; set; }
+    }
+    public class PostConfiguration : IEntityTypeConfiguration<Post>
+    {
+        public void Configure(EntityTypeBuilder<Post> builder)
+        {
+            builder.Property(p => p.Title).IsRequired().HasMaxLength(200);
+            builder.Property(p => p.Description).IsRequired().HasMaxLength(2000);
+            builder.HasOne(p => p.Category).WithMany(p=>p.Posts).HasForeignKey(p=>p.CategoryId);
+            builder.HasOne(p=>p.User).WithMany(p=>p.Posts).HasForeignKey(p=>p.UserId);
+        }
+    }
+}
---
+++ Entities/User/User.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities
+{
+    public class User : BaseEntity<Guid>
+    {
+        [Required]
+        [StringLength(100)]
+        public string UserName { get; set; }
+        [Required]
+        [StringLength(500)]
+        public string PasswordHash { get; set; }
+        [Required]
+        [StringLength(200)]
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        public GenderType Gender { get; set; }
+        public bool IsActive { get; set; }
+        public DateTimeOffset DateTimeOffset { get; set; }
+        public ICollection<Post> Posts { get; set; }
+    }
+    public enum GenderType
+    {
+        [Display(Name ="مرد")]
+        Male = 1,
+        [Display(Name = "زن")]
+        Female = 2
+    }
+}
