5c57f1d8364ded441b35d7fbeabdca2b389e6178
FarazJahanmanesh <jahanmaneshfaraz@gmail.com>
2023-11-01 14:27:15 +0330

    some Changes in project

---
+++ Common/Enums/ApiResultStatusCode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+S
+namespace WebFramework.Api
+{
+    public enum ApiResultStatusCode
+    {
+        [Display(Name="its success full")]
+        Success=200,
+        [Display(Name="error in system")]
+        ServerError = 503,
+        [Display(Name="wrong request")]
+        BadRequest = 400,
+        [Display(Name="error not found")]
+        NotFound= 404
+    }
+}
