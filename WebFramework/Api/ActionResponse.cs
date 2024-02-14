fa553ab91c692f38f978ebc6548a4516ebd50e5c
FarazJahanmanesh <jahanmaneshfaraz@gmail.com>
2024-02-14 16:47:17 +0330

    add the ActionResponse

--- WebFramework/Api/ActionResponse.cs
+++ WebFramework/Api/ActionResponse.cs
@@ -14,7 +14,8 @@ namespace WebFramework.Api
         {
             Errors = new List<string>();
         }
-
+        public int Status { get; set; }
+        public string Message { get; set; }
         public T Data { get; set; }
         public ResponseStateEnum State { get; set; }
         public List<string> Errors { get; set; }
