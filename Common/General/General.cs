6b585728b16c668d74d273d7246e817e96d09500
FarazJahanmanesh <jahanmaneshfaraz@gmail.com>
2024-04-07 08:07:19 +0330

    complete the GetConfigurationValue

--- Common/General/General.cs
+++ Common/General/General.cs
@@ -1,26 +1,16 @@
-﻿using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using System.Globalization;
-using System.Net;
-using System.Text.RegularExpressions;
-using System.Web;
+﻿#nullable disable
+using Microsoft.Extensions.Configuration;
 
-namespace Common.Genral
+namespace Common.Genral;
+
+public static class General
 {
-    public class General
+    public static IConfigurationRoot Configuration;
+    public static string GetConfigurationValue(string section)
     {
-        public static IConfigurationRoot Configuration;
-        public static string GetConfigurationValue(string section)
-        {
-            var builder = new ConfigurationBuilder().SetBasePath(System.IO.Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
-            Configuration = builder.Build();
-            var connectionString = Configuration.GetSection(section).Value.ToString();
-            return connectionString;
-        }
+        var builder = new ConfigurationBuilder().SetBasePath(System.IO.Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
+        Configuration = builder.Build();
+        var connectionString = Configuration.GetSection(section).Value.ToString();
+        return connectionString;
     }
 }
